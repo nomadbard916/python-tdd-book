8efb01cc25771233b6c00b24b2d458f140103f47
nomadbard916 <neonnova916@gmail.com>
2020-06-19 20:11:04 +0800

    initial setup

---
+++ functional_tests.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+# Edith has heard about a cool new online to-do app. She goes
+# to check out its homepage
+browser = webdriver.Chrome()
+browser.get("http://localhost:8000")
+
+# She notices the page title and header mention to-do lists
+
+
