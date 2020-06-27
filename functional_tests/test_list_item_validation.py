238cc85940338c7883bf5a946fbc31b0ad784252
nomadbard916 <neonnova916@gmail.com>
2020-06-27 16:45:56 +0800

    Use get_absolute_url on List model to DRY urls in views

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,18 +1,20 @@
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
-        #  Edith goes to the home page and accidentally tries to submit an empty list item. She hits Enter on the empty input box
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
-        # The home page refreshes, and there is an error message saying that list items cannot be blank
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
         self.wait_for(
             lambda: self.assertEqual(
                 self.browser.find_element_by_css_selector(".has-error").text,
-                r"You can't have an empty list item",
+                "You can't have an empty list item",
             )
         )
 
@@ -24,11 +26,11 @@ class ItemValidationTest(FunctionalTest):
         # Perversely, she now decides to submit a second blank list item
         self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
-        #  She receives a similar warning on the list page
+        # She receives a similar warning on the list page
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text(),
-                r"You can't have an empty list item",
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You can't have an empty list item",
             )
         )
 
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -38,22 +38,6 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class NewItemTest(TestCase):
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            f"/lists/{correct_list.id}/",
-            data={"item_text": "A new item for an existing list"},
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new item for an existing list")
-        self.assertEqual(new_item.list, correct_list)
-
-
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -106,3 +90,15 @@ class ListViewTest(TestCase):
             data={"item_text": "A new item for an existing list"},
         )
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+
+        response = self.client.post(f"/lists/{list_.id}/", data={"item_text": ""})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "list.html")
+
+        expected_error = escape("You can't have an empty list item")
+
+        self.assertContains(response, expected_error)
