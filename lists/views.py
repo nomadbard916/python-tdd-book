978bd23c134488307af0b756698f278b67bafba9
nomadbard916 <neonnova916@gmail.com>
2020-06-27 17:59:59 +0800

    typo: creating item before validation

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
-# Create your views here.
+
 def home_page(request):
     return render(request, "home.html")
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    item = Item(text=request.POST["item_text"], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, "home.html", {"error": error})
+    return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -13,27 +26,13 @@ def view_list(request, list_id):
 
     if request.method == "POST":
         try:
-            item = Item.objects.create(text=request.POST["item_text"], list=list_)
+
+            item = Item(text=request.POST["item_text"], list=list_)
             item.full_clean()
             item.save()
-
             return redirect(list_)
-
         except ValidationError:
             error = "You can't have an empty list item"
 
     return render(request, "list.html", {"list": list_, "error": error})
-
-
-def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST["item_text"], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = r"You can't have an empty list item"
-        return render(request, "home.html", {"error": error})
-    return redirect(list_)
 
