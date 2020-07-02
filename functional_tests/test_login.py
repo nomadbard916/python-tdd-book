5ebd6da26d754a4dd9c1498308dd7f82c912a7d5
nomadbard916 <neonnova916@gmail.com>
2020-07-02 12:11:25 +0800

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -14,7 +14,6 @@ class LoginTest(FunctionalTest):
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
         self.browser.get(self.live_server_url)
-
         self.browser.find_element_by_name("email").send_keys(TEST_EMAIL)
         self.browser.find_element_by_name("email").send_keys(Keys.ENTER)
 
@@ -34,8 +33,7 @@ class LoginTest(FunctionalTest):
         self.assertIn("Use this link to log in", email.body)
         url_search = re.search(r"http://.+/.+$", email.body)
         if not url_search:
-            self.fail(f"Could not find url in email body:")
-
+            self.fail(f"Could not find url in email body:\n{email.body}")
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -43,14 +41,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # she is logged in!
-        self.wait_for(lambda: self.browser.find_element_by_link_text("Log out"))
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Now she logs out
         self.browser.find_element_by_link_text("Log out").click()
 
         # She is logged out
-        self.wait_for(lambda: self.browser.find_element_by_name("email"))
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
