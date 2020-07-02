93795c197514f489063d69a21d609a86501d264b
nomadbard916 <neonnova916@gmail.com>
2020-07-02 16:26:31 +0800

    new_list view tries to assign owner but cant

--- lists/views.py
+++ lists/views.py
@@ -11,9 +11,10 @@ def home_page(request):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
-        Item.objects.create(text=request.POST["text"], list=list_)
-
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, "home.html", {"form": form})
